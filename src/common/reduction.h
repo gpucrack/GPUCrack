51b255466b6e121e6e066060416169229a8a0d17
Dorian Bucaille <dorian.bucaille@orange.fr>
2021-11-08 18:46:42 +0100

    Implementation of various reduction functions

---
+++ src/common/reduction.h
@@ -0,0 +1,9 @@
+#define PLAIN_LENGTH 8
+#define CHARSET_LENGTH 62
+
+void reduceV1(unsigned long int columnIndex, const char* hash, char* plain);
+void reduceV2(unsigned long int columnIndex, const char* hash, char* plain);
+void reduceV3(unsigned long int columnIndex, const char* hash, char* plain);
+void reduceV4(unsigned long int columnIndex, const char* hash, char* plain);
+void reduceV5(unsigned long int columnIndex, const char* hash, char* plain);
+//void reduceRainbowCrackalack(unsigned long int columnIndex, const char* hash, char* plain);
