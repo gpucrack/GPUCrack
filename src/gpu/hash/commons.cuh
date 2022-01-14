ef98becdb287e0fa3e11f8e9a91fabe1570782b3
Maxime-Missichini <Themynder@gmail.com>
2022-01-14 12:32:32 +0100

    Cleaning files

--- src/gpu/hash/commons.cuh
+++ src/gpu/hash/commons.cuh
@@ -1,21 +1,12 @@
-//
-// Created by mynder on 13/12/2021.
-//
-
 #ifndef CUDA_NAIVE_EXHAUSTIVE_SEARCH_COMMONS_CUH
 #define CUDA_NAIVE_EXHAUSTIVE_SEARCH_COMMONS_CUH
 
-
 #include "../constants.cuh"
-
-class commons {
-
-};
 
 __host__ double memoryAnalysis();
 __host__ int computeBatchSize(double numberOfPass);
 __host__ void readPasswords(const Password * h_passwords, const int batchSize);
-__host__ void kernel(const double numberOfPass, int batchSize, float * milliseconds, const clock_t * program_start,
+__host__ void kernel(double numberOfPass, int batchSize, float * milliseconds, const clock_t * program_start,
                      Digest ** h_results);
 
 
