c4a2b28a4e455d66ae47274abc225de102326cba
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-25 18:55:58 +0100

    Added semicolons to function declarations

--- src/common/reduction/reduction.h
+++ src/common/reduction/reduction.h
@@ -19,8 +19,10 @@
 // MUST BE A POWER OF 2
 #define THREAD_PER_BLOCK 512
 
+// One billion 1000000000
 // One hundred million 100000000
-#define DEFAULT_PASSWORD_NUMBER 1000000000
+// One thousand 1000
+#define DEFAULT_PASSWORD_NUMBER 1000
 
 // A macro to have a ceil-like function.
 #define CEILING(x, y) (((x) + (y)-1) / (y))
@@ -43,25 +45,25 @@ typedef union {
  * text: literal to be put into the password
  * password: result
 */
-void generate_pwd_from_text(char text[], Password *password)
+void generate_pwd_from_text(char text[], Password *password);
 
 /*
  * Displays a single password properly, char by char.
  * pwd: the password to display.
  */
-void display_password(const Password *pwd)
+void display_password(const Password *pwd);
 
 /*
  * Displays a password array properly with chars.
  * passwords: the password array to display.
  */
-void display_passwords(Password **passwords)
+void display_passwords(Password **passwords);
 
 /*
  * Displays a digest array properly with chars.
  * digests: the digest array to display.
  */
-void display_digests(Digest **digests)
+void display_digests(Digest **digests);
 
 /*
  * Generates a password corresponding to a given number.
@@ -69,32 +71,32 @@ void display_digests(Digest **digests)
  * counter: number corresponding to the chain's index
  * plain_text: password corresponding to its counter
 */
-void generate_password(unsigned long counter, Password &plain_text)
+void generate_password(unsigned long counter, Password &plain_text);
 
 /*
  * Fills passwords with n generated passwords.
  * passwords: the password array to fill.
  * n: the number of passwords to be generated.
  */
-void generate_passwords(Password **passwords, int n)
+void generate_passwords(Password **passwords, int n);
 
 /*
  * Generates a pseudo-random digest.
  * counter: this of it as a seed
  * hash: result digest
 */
-void generate_digest(unsigned long counter, Digest &hash)
+void generate_digest(unsigned long counter, Digest &hash);
 
 /*
  * Fills digests with n generated digests.
  * digests: the digest array to fill.
  * n: the number of digests to be generated.
  */
-void generate_digests(Digest **digests, int n)
+void generate_digests(Digest **digests, int n);
 
 /*
  * Reduces every digest of an array into plain texts.
  * digests: the digest array to reduce
  * plain_texts: the generated reductions
  */
-void reduce_digests(Digest **digests, Password **plain_texts)
+void reduce_digests(Digest **digests, Password **plain_texts);
