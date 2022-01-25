bbbae8ae28b4955f33ec98dbf42ac81c3c3daad6
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-01-25 16:24:18 +0100

    Collision test #1

--- src/common/reduction/reduction.cpp
+++ src/common/reduction/reduction.cpp
@@ -1,6 +1,7 @@
 #include "reduction.h"
 
-char *charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";   // the characters to be used in the generated plain text
+// The character set used.
+static const char *charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_";
 
 /*
  * Reduces a hash into a plain text of length PLAIN_LENGTH.
@@ -9,27 +10,60 @@ char *charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
  * plain: result of the reduction
  */
 void reduce(unsigned long int index, const char *hash, char *plain) {
-    for (unsigned long int i = 0; i < PLAIN_LENGTH; i++, plain++, hash++)
+    for (unsigned long int i = 0; i < PASSWORD_LENGTH; i++, plain++, hash++)
         *plain = charset[(unsigned char) (*hash ^ index) % CHARSET_LENGTH];
+}
+
+// May be useless tbh
+char char_in_range(unsigned char n) {
+    return charset[n];
+}
+
+// Generate passwords
+void create_startpoint(unsigned long counter, Password *plain_text) {
+    for (int i = PASSWORD_LENGTH - 1; i >= 0; i--) {
+        plain_text->bytes[i] = charset[counter % CHARSET_LENGTH];
+        counter /= CHARSET_LENGTH;
+    }
+}
+
+/* Generates a password given a char array.
+* text: cypher text to be reduced
+* plain: result of the reduction
+*/
+void generate_pwd(char text[], Password *password) {
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        password->bytes[i] = text[i];
+    }
+}
+
+void reduce_digest(Digest *digest, unsigned long iteration, unsigned char table_number, Password *plain_text) {
+    // pseudo-random counter based on the hash
+    unsigned long counter = digest->bytes[7];
+    for (char i = 6; i >= 0; i--) {
+        counter <<= 8;
+        counter |= digest->bytes[i];
+    }
 }
 
 /*
  * Tests the reduction and displays it in the console.
  */
 int main() {
-    unsigned int index = 3;
-    unsigned int index2 = 4;
+    Password *pwd = NULL;
+    pwd = (Password *) malloc(sizeof(Password));
 
-    char *hash = "8846F7EAEE8FB117AD06BDD830B7586C";
-    char *hash2 = "878D8014606CDA29677A44EFA1353FC7";
-    char *plain = malloc(sizeof(char) * (PLAIN_LENGTH));
-    char *plain2 = malloc(sizeof(char) * (PLAIN_LENGTH));
+    char test[] = "123456789";
 
-    reduce(index, hash, plain);
-    reduce(index2, hash2, plain2);
+    printf("%lu\n", sizeof test);
 
-    printf("%s ---> %s\n", hash, plain);
-    printf("%s ---> %s\n", hash2, plain2);
+    generate_pwd(test, pwd);
+
+
+    for (unsigned char byte: pwd->bytes) {
+        printf("%c", (char) byte);
+    }
+    printf("\n");
 
     return 0;
 }
--- src/common/reduction/reduction.h
+++ src/common/reduction/reduction.h
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdint>
 
-#define PLAIN_LENGTH 8  // the length of the generated plain text
-#define CHARSET_LENGTH 62   // the number of characters in the charset
+// The length of the hash function.
+#define HASH_LENGTH 16
+
+// The length of a password.
+#define PASSWORD_LENGTH 7
+
+#define CHARSET_LENGTH 64
+
+// How many thread per block to launch the kernel
+// MUST BE A POWER OF 2
+#define THREAD_PER_BLOCK 512
+
+#define DEFAULT_PASSWORD_NUMBER 1073741824
+
+// A macro to have a ceil-like function.
+#define CEILING(x, y) (((x) + (y)-1) / (y))
+
+// A password put into a union. This is easier to use with malloc and crypto
+// functions.
+typedef union {
+    uint8_t bytes[PASSWORD_LENGTH];
+    uint32_t i[CEILING(PASSWORD_LENGTH, 4)];
+} Password;
+
+// A digest put into an union.
+typedef union {
+    uint8_t bytes[HASH_LENGTH];
+    uint32_t i[CEILING(HASH_LENGTH, 4)];
+} Digest;
 
 // Reduces a hash into a plain text of length PLAIN_LENGTH.
 void reduce(unsigned long int index, const char *hash, char *plain);
