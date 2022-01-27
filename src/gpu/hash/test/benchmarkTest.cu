635eaf7c14239a6bfe01414757bf0599df9a133b
Maxime-Missichini <Themynder@gmail.com>
2022-01-27 14:45:52 +0100

    Fixed benchmark

--- src/gpu/hash/test/benchmarkTest.cu
+++ src/gpu/hash/test/benchmarkTest.cu
@@ -1,10 +1,9 @@
 #include "benchmarkTest.cuh"
 
 void benchmark(int passwordNumber) {
-    double maxHashRate;
-    double hashRateSum;
-    double bestHashRateMean;
-    int bestValue;
+    double maxHashRate = 0;
+    double bestHashRateMean = 0;
+    int bestValue = 0;
 
     Password * passwords;
     Digest * result;
@@ -14,6 +13,9 @@ void benchmark(int passwordNumber) {
     auto numberOfPass = memoryAnalysis(passwordNumber);
 
     for (int k = 2; k <= MAX_THREAD_NUMBER; k = k * 2) {
+
+        double hashRateSum = 0;
+
         for (int i = 0; i < NUMBER_OF_TEST; i++) {
 
             float milliseconds = 0;
@@ -21,7 +23,7 @@ void benchmark(int passwordNumber) {
             hashTime(passwords, result, passwordNumber, &milliseconds, k,
                      numberOfPass);
 
-            double hashrate = ((float)passwordNumber / (milliseconds / 1000)) / 1000000;
+            double hashrate = (passwordNumber / (milliseconds / 1000)) / 1000000;
 
             if (hashrate > maxHashRate) {
                 maxHashRate = hashrate;
