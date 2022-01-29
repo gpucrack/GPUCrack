ea5503cdf8509a958af3a35d8f6e7460952740c0
Maxime-Missichini <Themynder@gmail.com>
2022-01-29 18:51:14 +0100

    finished chainsComplianceTest.cu

--- src/gpu/test/chainsComplianceTest.cu
+++ src/gpu/test/chainsComplianceTest.cu
@@ -14,17 +14,32 @@ void chainCompliance(int passwordNumber, Password * passwords, Digest * result, 
 
     printf("\n==========COMPLIANCE TEST==========\n");
 
-    Password * referencePassword = &(passwords[passwordNumber-1]);
-    Digest * referenceDigest = &(result[passwordNumber-1]);
+    printf("COMPARING ALL RESULTS TO REFERENCE RESULT\n");
 
-    printf("RESULTS FROM BASE FUNCTION : \n");
-    printPassword(referencePassword);
-    printf("\n");
-    hash(referencePassword, referenceDigest, 1, 1);
-    printf("\nBECOMES:\n");
-    printDigest(referenceDigest);
-    printf("\n\n");
+    for (int i = 0; i < 1000; i++) {
 
+        Password * referencePassword = &(passwords[i]);
+        auto * referenceDigest = (Digest *)malloc(sizeof(Digest));
+
+        hash(referencePassword, referenceDigest, 1, 1);
+
+        int comparison = memcmp((result[i]).bytes, (*referenceDigest).bytes, HASH_LENGTH);
+        if (comparison != 0) {
+            printf("TEST FAILED ! %d\n", comparison);
+            printf("FAILED @ DIGEST N°%d\n", i);
+            printDigest(referenceDigest);
+            printf("THIS IS THE FAIL SAMPLE: ");
+            printDigest(&(result[i]));
+            printf("\n");
+            exit(1);
+        }
+
+        referencePassword = nullptr;
+
+        free(referencePassword);
+        free(referenceDigest);
+    }
+    printf("TEST PASSED !\n");
 }
 
 int main() {
