8ced8d5399dcbdbbd3497f9a7e4c6f3227d6d04d
Maxime-Missichini <Themynder@gmail.com>
2022-02-04 13:07:24 +0100

    Deleted compliance tests on chain since they are useless

--- src/gpu/test/chainsComplianceTest.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef GPU_CRACK_CHAINSCOMPLIANCETEST_CUH
-#define GPU_CRACK_CHAINSCOMPLIANCETEST_CUH
-
-#include "../commons.cuh"
-
-void chainCompliance(int passwordNumber, Password * passwords, Digest * result, int numberOfPass);
-
-#endif //GPU_CRACK_CHAINSCOMPLIANCETEST_CUH
