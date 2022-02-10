8ce7a8d0b1c32bf0a2b69d45eceda1c132b24db0
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-02-10 16:35:24 +0100

    Added device analysis

--- src/gpu/commons.cu
+++ src/gpu/commons.cu
@@ -40,6 +40,30 @@ __host__ int memoryAnalysis(int passwordNumber) {
 
     printf("\n==========MEMORY ANALYSIS==========\n");
 
+    int nDevices;
+    cudaGetDeviceCount(&nDevices);
+
+    printf("Number of devices: %d\n", nDevices);
+
+    for (int i = 0; i < nDevices; i++) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("Device Number: %d\n", i);
+        printf("  Device name: %s\n", prop.name);
+        printf("  Memory Clock Rate (MHz): %d\n",
+               prop.memoryClockRate/1024);
+        printf("  Memory Bus Width (bits): %d\n",
+               prop.memoryBusWidth);
+        printf("  Peak Memory Bandwidth (GB/s): %.1f\n",
+               2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+        printf("  Total global memory (Gbytes) %.1f\n",(float)(prop.totalGlobalMem)/1024.0/1024.0/1024.0);
+        printf("  Shared memory per block (Kbytes) %.1f\n",(float)(prop.sharedMemPerBlock)/1024.0);
+        printf("  minor-major: %d-%d\n", prop.minor, prop.major);
+        printf("  Warp-size: %d\n", prop.warpSize);
+        printf("  Concurrent kernels: %s\n", prop.concurrentKernels ? "yes" : "no");
+        printf("  Concurrent computation/communication: %s\n\n",prop.deviceOverlap ? "yes" : "no");
+    }
+
     // Checking if THREAD_PER_BLOCK is a power of 2 because we will have memory problems otherwise
     if ((ceil(log2(THREAD_PER_BLOCK)) != floor(log2(THREAD_PER_BLOCK)))) {
         printf("THREAD PER BLOCK VALUE IS NOT A POWER OF 2 !\n");
