307ea6c918f9dc4ff53cab56aeb3b2ba79bf6403
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-02-10 17:51:12 +0100

    Made only one thread to init loading bar

--- src/gpu/chains.cu
+++ src/gpu/chains.cu
@@ -103,7 +103,7 @@ __global__ void ntlm_chain_kernel(Password *passwords, Digest *digests, int chai
 
     const unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
     double progress = 1.;
-    initLoadingBar();
+    if (index == 0) initLoadingBar();
 
     for (int i = 0; i < chainLength; i++) {
 
