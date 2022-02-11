3326442fdeb4c1b232aa703dd5c7713f6c12c09c
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-02-11 10:10:18 +0100

    Re-added print buffering

--- src/gpu/test/generateTableTest.cu
+++ src/gpu/test/generateTableTest.cu
@@ -6,8 +6,6 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    setvbuf(stdout, NULL, _IONBF, 0); // Disables buffer in console (for progress bar)
-
     printSignature();
 
     int passwordNumber = getM0(getTotalSystemMemory(), atoi(argv[1]));
