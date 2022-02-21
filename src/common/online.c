5b9794f5f0b7572b7dc20376359ac22f48fa5865
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-02-21 15:07:49 +0100

    Experiment number 239451

--- src/common/online.c
+++ src/common/online.c
@@ -341,7 +341,7 @@ void online_from_files(char *start_path, char *end_path, unsigned char *digest, 
 
     for (long i = t - 1; i >= 0; i--) {
 
-        printf("\r%.2f %%", ((double)(t-1-i)/(double)t-1) * 100);
+        printf("\r%.2f %%", 1/(((double)(i)/(double)t-1) * 100));
 
         char column_plain_text[pwd_length + 1];
         unsigned char column_digest[HASH_LENGTH];
