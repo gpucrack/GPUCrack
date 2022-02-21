48f05d6ddbd6960adff5e80f1ba6a7db8f25dd93
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-02-21 18:52:13 +0100

    Experiment number 2394538

--- src/common/online.c
+++ src/common/online.c
@@ -393,7 +393,7 @@ void online_from_files(char *start_path, char *end_path, unsigned char *digest, 
         }
         ntlm(chain_plain_text, chain_digest);
 
-        printf("FALSE ALERT ???????? C'EST : %s et %s\n", chain_digest, digest);
+        // printf("FALSE ALERT ???????? C'EST : %s et %s\n", chain_digest, digest);
 
         if (!memcmp(chain_digest, digest, HASH_LENGTH)) {
             strcpy(password, chain_plain_text);
