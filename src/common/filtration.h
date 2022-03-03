1ea6d9ee45fd0b6d17bffe4999e8558b3dff2d82
dorian-bucaille <dorian.bucaille@orange.fr>
2022-03-03 19:30:46 +0100

    Added table filtration.

---
+++ src/common/filtration.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Compares two strings.
+ * @param v1 the first string to compare.
+ * @param v2 the second string.
+ * @param len the number of bytes to compare.
+ * @return
+ */
+int cmpstr(void *v1, void *v2, int len);
+
+/**
+ * Swaps two elements.
+ * @param v1 the first element to swap.
+ * @param v2 the second element.
+ * @param size the number of bytes to swap.
+ */
+void swap(void *v1, void *v2, long size);
+
+/**
+ * Sorts an array.
+ * @param v array of elements to sort.
+ * @param m (mirror) array of elements to sort, like v.
+ * @param size the number of elements in sub-array (pwd_len).
+ * @param left start of array.
+ * @param right end of array.
+ * @param comp pointer to the comparison function.
+ */
+void q_sort(void *v, void *m, long size, long left, long right, int (*comp)(void *, void *, int));
+
+/**
+ * Deletes all duplicates in an array.
+ * @param v array of elements to deduplicate.
+ * @param m (mirror) array of elements to deduplicate, like v.
+ * @param size the number of elements in sub-array (pwd_len).
+ * @param mtthe size of v.
+ * @param comp a pointer to the comparison function.
+ * @return the number of startpoints written after deduplication
+ */
+long dedup(void *v, void *m, int size, long mt, int (*comp)(void *, void *, int));
+
+/**
+ * Cleans a table by deleting all of its duplicate endpoints.
+ * @param start_path the path to the startpoint file.
+ * @param end_path the path to the startpoint file.
+ * @param start_out_path the path to the clean startpoint file that will be generated.
+ * @param end_out_path the path to the clean startpoint file that will be generated.
+ * @return a long array containing :
+ *          - the initial number of endpoints;
+ *          - the new number of unique points;
+ *          - the number of startpoints written after deduplication;
+ *          - the number of endpoints written after deduplication.
+ */
+long *filter(const char *start_path, const char *end_path, const char *start_out_path, const char *end_out_path);
--- src/gpu/test/generateTableTest.cu
+++ src/gpu/test/generateTableTest.cu
@@ -1,6 +1,6 @@
 #include "generateTableTest.cuh"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Error: not enough arguments given.\n Usage: 'generateTable mt', where mt is the desired number of end points.");
         exit(1);
@@ -10,8 +10,8 @@ int main(int argc, char *argv[]){
 
     int passwordNumber = getM0(getTotalSystemMemory(), atoi(argv[1]));
 
-    Password * passwords;
-    Digest * result;
+    Password *passwords;
+    Digest *result;
 
     initArrays(&passwords, &result, passwordNumber);
 
@@ -21,13 +21,18 @@ int main(int argc, char *argv[]){
 
     printf("Number of columns: %d\n\n", t);
 
-    generateChains(passwords, result, passwordNumber, numberOfPass, t,
-                   true, THREAD_PER_BLOCK, false, false);
+    generateChains(passwords, result, passwordNumber, numberOfPass, t, true, THREAD_PER_BLOCK, false, false);
 
-    printf("Chains generated!\n");
+    printf("Chains generated!\n\n");
 
     cudaFreeHost(passwords);
     cudaFreeHost(result);
+
+    // Clean the table by deleting duplicate endpoints
+    long *res = filter("testStart.bin", "testEnd.bin", "testStart.bin", "testEnd.bin");
+    if (res[2] == res[3]) {
+        printf("The files have been generated with success.\n");
+    }
 
     return 0;
 }
