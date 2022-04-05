838499c100dafd2d2c0dfaa22a932ea65035e2c9
dorian-bucaille <dorian.bucaille@orange.fr>
2022-04-05 11:03:23 +0200

    Multiple files can be generated (same tables)

--- src/gpu/test/generateTableTest.cu
+++ src/gpu/test/generateTableTest.cu
@@ -1,20 +1,19 @@
 #include "generateTableTest.cuh"
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
+    if (argc < 4) {
         printf("Error: not enough arguments given.\n"
-               "Usage: 'generateTable c mt (startpath) (endpath)', where:\n"
+               "Usage: 'generateTable c n mt (path)', where:\n"
                "     - c is the passwords' length (in characters).\n"
+               "     - n is the number of tables to generate.\n"
                "     - mt is the number of end points to be generated.\n"
-               "     - (optional) startpath is the path to the start points file to create.\n"
-               "     - (optional) endpath is the path to the end points file to create.\n");
+               "     - (optional) path is the path to the start and end points files to create.\n");
         exit(1);
     }
 
     printSignature();
 
-    char *start_path;
-    char *end_path;
+    char *path;
     int pwd_length = atoi(argv[1]);
 
     long domain = pow(CHARSET_LENGTH, pwd_length);
@@ -23,13 +22,15 @@ int main(int argc, char *argv[]) {
 
     long idealMtMax = (long)((double)((double)idealM0/(double)19.83));
 
-    long mtMax = getNumberPassword(atoi(argv[2]), pwd_length);
+    long mtMax = getNumberPassword(atoi(argv[3]), pwd_length);
 
     mtMax = idealMtMax;
 
     long passwordNumber = idealM0;
 
     int t = computeT(mtMax, pwd_length);
+
+    int tableNumber = atoi(argv[2]);
 
     printf("mtMax: %ld\n", mtMax);
 
@@ -38,29 +39,19 @@ int main(int argc, char *argv[]) {
     printf("Password length: %d\n", pwd_length);
     printf("Number of columns (t): %d\n\n", t);
 
-    // User typed 'generateTable c mt'
-    if (argc == 3) {
-        start_path = (char *) "testStart.bin";
-        end_path = (char *) "testEnd.bin";
+    // User typed 'generateTable c n mt'
+    if (argc == 4) {
+        path = (char *) "test";
     }
-
-    Password * passwords;
 
     auto numberOfCPUPass = memoryAnalysisCPU(passwordNumber, getNumberPassword(getTotalSystemMemory()-9, pwd_length));
 
     printf("Number of CPU passes: %d\n", numberOfCPUPass);
 
     long batchSize = computeBatchSize(numberOfCPUPass, passwordNumber);
-    // User typed 'generateTable c mt startpath'
-    if (argc == 4) {
-        start_path = argv[3];
-        end_path = (char *) "testEnd.bin";
-    }
-
-    // User typed 'generateTable c mt startpath endpath'
+    // User typed 'generateTable c n mt path'
     if (argc == 5) {
-        start_path = argv[3];
-        end_path = argv[4];
+        path = argv[4];
     }
 
     printf("CPU batch size: %ld\n", batchSize);
@@ -69,39 +60,58 @@ int main(int argc, char *argv[]) {
 
     printf("Number of crypto op: %ld\n", nbOp);
 
-    initPasswordArray(&passwords, batchSize);
+    for(int table=0; table < tableNumber; table++) {
 
-    long currentPos = 0;
+        // Generate file name according to table number
+        char startName[100];
+        char endName[100];
+        strcpy(startName, path);
+        strcat(startName, "_start_");
+        strcpy(endName, path);
+        strcat(endName, "_end_");
+        char tableChar[10];
+        sprintf(tableChar, "%d", table);
+        strcat(startName, tableChar);
+        strcat(startName, ".bin");
+        strcat(endName, tableChar);
+        strcat(endName, ".bin");
 
-    for(int i=0; i<numberOfCPUPass; i++) {
+        // Initialize the passwords array
+        Password * passwords;
+        initPasswordArray(&passwords, batchSize);
 
-        printf("current position: %ld\n", currentPos);
+        long currentPos = 0;
 
-        if (currentPos == 0) createFile(start_path, true);
-        writePoint(start_path, &passwords, batchSize, t, pwd_length, true, currentPos);
+        for(int i=0; i<numberOfCPUPass; i++) {
 
-        auto numberOfPass = memoryAnalysisGPU(batchSize);
+            printf("current position: %ld\n", currentPos);
 
-        generateChains(passwords, batchSize, numberOfPass, t,
-                       true, THREAD_PER_BLOCK, false, false, NULL, pwd_length, start_path, end_path);
+            if (currentPos == 0) createFile(startName, true);
+            writePoint(startName, &passwords, batchSize, t, pwd_length, true, currentPos);
 
-        printf("Chains generated!\n");
+            auto numberOfPass = memoryAnalysisGPU(batchSize);
 
-        if (currentPos == 0) createFile(end_path, true);
-        writePoint(end_path, &passwords, batchSize, t, pwd_length, true, currentPos);
+            generateChains(passwords, batchSize, numberOfPass, t,
+                           true, THREAD_PER_BLOCK, false, false, NULL, pwd_length, startName, endName);
 
-        currentPos += batchSize;
+            printf("Chains generated!\n");
+
+            if (currentPos == 0) createFile(endName, true);
+            writePoint(endName, &passwords, batchSize, t, pwd_length, true, currentPos);
+
+            currentPos += batchSize;
+        }
+        printf("Engaging filtration...\n");
+
+        // Clean the table by deleting duplicate endpoints
+        long *res = filter(startName, endName, startName, endName);
+        if (res[2] == res[3]) {
+            printf("Filtration done!\n\n");
+            printf("The files have been generated with success.\n");
+        }
+
+        cudaFreeHost(passwords);
     }
-    printf("Engaging filtration...\n");
-
-    // Clean the table by deleting duplicate endpoints
-    long *res = filter(start_path, end_path, start_path, end_path);
-    if (res[2] == res[3]) {
-        printf("Filtration done!\n\n");
-        printf("The files have been generated with success.\n");
-    }
-
-    cudaFreeHost(passwords);
 
     return 0;
 }
