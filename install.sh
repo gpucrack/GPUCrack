7839a47c0d771636de1722293f43f6bf54deb151
dorian-bucaille <dorian.bucaille@orange.fr>
2022-05-01 23:09:51 +0200

    Added installation script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# This script will install GPUCrack on your system.
+
+printf "GPUCrack v0.1.4\n\nThis script will install GPUCrack on your system based on your choices.\n\n"
+printf "Do you want to install the GPUCrack NTLM table generator? (CUDA needed) (y/n): "
+read install_gpucrack_ntlm
+if [ "$install_gpucrack_ntlm" = "y" ]; then
+    printf "Installing the GPUCrack NTLM table generator...  "
+fi
+if [ "$install_gpucrack_ntlm" = "n" ]; then
+    printf "Installing the GPUCrack attack program only...  "
+    rm ./CMakeLists.txt
+    printf "add_executable(online src/common/online.c)\ntarget_link_libraries(online PRIVATE m)\n" >> ./CMakeLists.txt
+fi
+
+# get output of command 'cmake .'
+error_check=$(cmake .)
+if [[ "$error_check" == *"The CUDA compiler identification is unknown"* || "$error_check" == *"Failed to detect a default CUDA architecture."* ]]; then
+    printf "\n\nCUDA not found. Trying to locate NVCC...\n\n"
+    nvcc_path=$(which nvcc)
+    echo "nvcc_path: $nvcc_path"
+    if [ -z "$nvcc_path" ]; then
+        printf "NVCC not found. Please install CUDA and try again.\n"
+        exit 1
+    fi
+    rm -rf ./CMakeFiles
+    rm ./CMakeCache.txt
+    wait
+    error_check=$(cmake -DCMAKE_CUDA_COMPILER:PATH=$nvcc_path .)
+fi
+
+if [[ "$error_check" == *"-- Build files have been written"* ]]; then
+    error_check=$(cmake --build .)
+    if [[ "$error_check" == *"error"* ]]; then
+        printf "\n\nBuild failed.\n\n"
+        exit 1
+    fi
+    printf "Done.\n\nGPUCrack is now installed on your system.\n\nUse the following command to get started:\n\n"
+    printf "  ./online -h\n ./generateTable\n"
+    exit 0
+fi
+
+printf "\n\nError encountered while trying to install.\n\n"
+exit 1
