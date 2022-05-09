b00656f87657e3cd9d3a32ca0055f1bd3db9072e
Maxime-Missichini <Themynder@gmail.com>
2022-05-09 16:41:58 +0200

    Fixed typo

--- src/gpu/test/generateTableTest.cu
+++ src/gpu/test/generateTableTest.cu
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
                "Usage: 'generateTable c n mt (path)', where:\n"
                "     - c is the passwords' length (in characters).\n"
                "     - n is the number of tables to generate.\n"
-               "     - p is the percentage of the password's length domain to be used as m0.\n"
+               "     - p is the ratio of the password's length domain to be used as m0.\n"
                "     - (optional) path is the path to the start and end points files to create.\n");
         exit(1);
     }
